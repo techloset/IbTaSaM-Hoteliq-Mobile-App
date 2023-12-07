b16c416060e3565b6cc1dfd9e7089f366c96f6a1
ibtasam116 <ibtasam116@gmail.com>
2023-12-07 21:42:52 +0500

    fix

---
+++ src/library/SCREENS.js
@@ -0,0 +1,21 @@
+const SCREENS = {
+  HOME: 'home',
+  SCHDULE: 'schdule',
+  ACCOUNT: 'account',
+  PROFILE: 'profile',
+  DETAIL: 'detail',
+  // ONBOARDING: 'Onboarding',
+  // LOGIN: 'Login',
+  // SIGN_UP: 'SignUp',
+  // OTP: 'OTP',
+  // OTP_CHA: 'OTPCha',
+  // PRODUCTS: 'Products',
+  // // BOTTOM_NAVIGATOR
+  // HOME: 'Home',
+  // BROWSE: 'Browse',
+  // STORE: 'Store',
+  // ORDER: 'Order',
+  // PROFILE: 'Profile',
+};
+
+export default SCREENS;
