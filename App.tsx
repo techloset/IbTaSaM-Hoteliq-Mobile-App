5db8c6acd511c2ffa045a928174829bbe6f1c7bf
ibtasam116 <ibtasam116@gmail.com>
2023-12-19 13:10:06 +0500

    WorK

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import Navigation from './src/navigation/Navigation';
 export default function App() {
-  return (<Navigation />);
+  return <Navigation />;
 }
--- src/components/searchButton/SearchButton.jsx
+++ src/components/searchButton/SearchButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
 import House from '../../assets/svg/house.svg';
+import HouseWhite from '../../assets/svg/houseWhite.svg';
 import Shop from '../../assets/svg/shop.svg';
+import ShopWhite from '../../assets/svg/shopWhite.svg';
 import Coffee from '../../assets/svg/coffee.svg';
+import CoffeeWhite from '../../assets/svg/coffeeWhite.svg';
 
 import COLORS from '../../consts/Color';
 
@@ -16,26 +19,56 @@ import {
 } from '../../utils/ResponsiveStyle';
 
 export const SearchButton = () => {
+  const [color, setColor] = useState(true);
+  const [color1, setColor1] = useState(false);
+  const [color2, setColor2] = useState(false);
+  const setColorHandler = () => {
+    setColor(true);
+    setColor1(false);
+    setColor2(false);
+  };
+  const setColor1Handler = () => {
+    setColor(false);
+    setColor1(true);
+    setColor2(false);
+  };
+  const setColor2Handler = () => {
+    setColor(false);
+    setColor1(false);
+    setColor2(true);
+  };
   return (
     <>
       <View style={styles.mainStyle}>
-        <TouchableOpacity style={styles.PressableHouse}>
-          <House width={widthPixel(20)} height={heightPixel(20)} />
-          <Text style={{color: COLORS.white, fontSize: fontPixel(14)}}>
-            Hotel
-          </Text>
+        <TouchableOpacity
+          style={color ? styles.PressableColor : styles.PressableBtn}
+          onPress={setColorHandler}>
+          {color ? (
+            <HouseWhite width={widthPixel(20)} height={heightPixel(20)} />
+          ) : (
+            <House width={widthPixel(20)} height={heightPixel(20)} />
+          )}
+          <Text style={color ? styles.titlePress : styles.title}>Hotel</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.PressableBtn}>
-          <Shop width={widthPixel(20)} height={heightPixel(20)} />
-          <Text style={{fontSize: fontPixel(14), color: COLORS.grey}}>
-            Market
-          </Text>
+        <TouchableOpacity
+          style={color1 ? styles.PressableColor : styles.PressableBtn}
+          onPress={setColor1Handler}>
+          {color1 ? (
+            <ShopWhite width={widthPixel(20)} height={heightPixel(20)} />
+          ) : (
+            <Shop width={widthPixel(20)} height={heightPixel(20)} />
+          )}
+          <Text style={color1 ? styles.titlePress : styles.title}>Market</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.PressableBtn}>
-          <Coffee width={widthPixel(20)} height={heightPixel(20)} />
-          <Text style={{fontSize: fontPixel(14), color: COLORS.grey}}>
-            Coffee
-          </Text>
+        <TouchableOpacity
+          style={color2 ? styles.PressableColor : styles.PressableBtn}
+          onPress={setColor2Handler}>
+          {color2 ? (
+            <CoffeeWhite width={widthPixel(20)} height={heightPixel(20)} />
+          ) : (
+            <Coffee width={widthPixel(20)} height={heightPixel(20)} />
+          )}
+          <Text style={color2 ? styles.titlePress : styles.title}>Coffee</Text>
         </TouchableOpacity>
       </View>
     </>
@@ -48,7 +81,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     gap: 12,
   },
-  PressableHouse: {
+  title: {fontSize: fontPixel(14), color: COLORS.grey},
+  titlePress: {color: COLORS.white},
+  PressableColor: {
     backgroundColor: COLORS.lightBlue,
     borderRadius: 8,
     flexDirection: 'row',
